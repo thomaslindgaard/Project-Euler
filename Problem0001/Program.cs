fa8389e1ac1c15d2260871f34ab745cb3921a652
Thomas Lindgaard <thomas.lindgaard@gmail.com>
2025-08-25 15:29:42 +0200

    Problem 1 solved

---
+++ Problem0001/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Find sum of multiples of 3 and 5 below 1.000");
+
+//var multiples = Enumerable.Range(1, 999).Where(n => n % 3 == 0 || n % 5 == 0);
+var multiples = Enumerable.Range(1, 999).Where(n => n % 5 == 0);
+var sum = multiples.Sum();
+Console.WriteLine($"Sum: {sum}");
