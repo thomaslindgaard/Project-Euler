c0ce8cb3636aee748c00442fb778be46b3bcbcba
Thomas Lindgaard <thomas.lindgaard@gmail.com>
2025-08-25 15:34:14 +0200

    Remove debug stuff from Problem 1 solution

--- Problem0001/Program.cs
+++ Problem0001/Program.cs
@@ -2,7 +2,6 @@
 
 Console.WriteLine("Find sum of multiples of 3 and 5 below 1.000");
 
-//var multiples = Enumerable.Range(1, 999).Where(n => n % 3 == 0 || n % 5 == 0);
-var multiples = Enumerable.Range(1, 999).Where(n => n % 5 == 0);
+var multiples = Enumerable.Range(1, 999).Where(n => n % 3 == 0 || n % 5 == 0);
 var sum = multiples.Sum();
 Console.WriteLine($"Sum: {sum}");
